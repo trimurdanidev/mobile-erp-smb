f2819402b27f005b23f18d04d97f60fa711a89dc
Trimurdanidev <chersman82@gmail.com>
2025-10-25 00:21:43 +0700

    update vite https

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+  GNU nano 4.8                                                                 vite.config.ts
 /// <reference types="vitest" />
 
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +21,13 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom'
+  },
+  server: {
+    host: true,
+    https: {
+      key: fs.readFileSync('/etc/nginx/certs/erpsmb.key'),
+      cert: fs.readFileSync('/etc/nginx/certs/ssl_bundle.crt')
+    },
+    allowedHosts: ['erpsmb.cloud']
   }
 })
